3cb23dc744b0f0b87e2c873a5f5747352ecb99a7
EmilioMena <eamean@uwaterloo.ca>
2019-09-16 22:03:49 -0400

    Navbar

---
+++ src/app/sort-option/sort-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-option',
+  templateUrl: './sort-option.component.html',
+  styleUrls: ['./sort-option.component.css']
+})
+export class SortOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
